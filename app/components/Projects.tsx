986a8cd46ccaa0211fac58d077d544570c383c0f
zunair2002 <zunairshahid02@gmail.com>
2026-05-30 18:39:00 +0500

    complete2

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -11,7 +11,7 @@ const demoprojects = [
     title: "SabziMart",
     description:
       "SabziMart is a grocery delivery platform built for customers riders and admins on a secure and unified system. It provides a reliable end to end workflow from order to delivery. The platform is designed for scalability performance and a smooth user experience.",
-    tags: ["React", "Node.js", "MongoDB", "Express", "JWT"],
+    tags: ["React", "TypeScript", "MongoDB", "Express", "Socket.IO","Leftlet","Cloudinary","NextAuth"],
     githubUrl: "https://github.com/zunair2002/SabziMart",
     demoUrl: "/demoprojects/1",
   },
@@ -19,7 +19,7 @@ const demoprojects = [
     id: 2,
     title: "ChatGO",
     description:
-      "A real-time chat application with Socket.io messaging and secure authentication, featuring a responsive interface and a scalable backend for smooth, reliable communication.",
+"A real-time chat application with Socket.io enables instant messaging through a live connection between users and the server. It uses secure authentication to protect user data and ensure only verified access. The system is responsive and scalable, delivering smooth performance across all devices.",
     tags: ["React", "Socket.io", "Express", "JWT", "MongoDB"],
     githubUrl: "https://github.com/zunair2002/ChatGO",
     demoUrl: "/demoprojects/2",
@@ -28,7 +28,7 @@ const demoprojects = [
     id: 3,
     title: "WalletWave",
     description:
-      "A modern crypto-wallet interface that shows live cryptocurrency data and key info for popular coins. It also features an interactive Bitcoin chart to track market trends in real time.",
+"A modern crypto-wallet interface that displays live cryptocurrency data and key information for popular coins in real time. It includes an interactive Bitcoin chart that helps users track market trends and price movements. The system provides a clean and responsive design for smooth user experience across devices.",
     tags: ["React", "CoinGecko API", "Chart.js", "Tailwind"],
     githubUrl: "https://github.com/zunair2002/WalletWave",
     demoUrl: "/demoprojects/3",
@@ -38,7 +38,7 @@ const demoprojects = [
     title: "Expense Tracker",
     description:
       "An expense tracker logs all your income and spending using simple charts to show exactly where your money goes. This helps you easily understand your financial habits and make smarter plans for your budget.",
-    tags: ["React", "LocalStorage", "Recharts", "Tailwind"],
+    tags: ["React", "LocalStorage", "MongoDB", "Express", "Recharts", "Tailwind"],
     githubUrl: "https://github.com/zunair2002/ExpenseApp-MERN",
     demoUrl: "/demoprojects/4",
   },
@@ -46,9 +46,8 @@ const demoprojects = [
   {
     id: 5,
     title: "Email Checker",
-    description:
-      "AI SQL Agent Built an AI SQL Agent that converts natural language into SQL queries and executes database operations. Integrated AI SDK with xAI Grok API to enable seamless and intelligent database interactions.",
-    tags: ["React", "LocalStorage", "Recharts", "Tailwind"],
+    description:`AI-integrated checkers evaluate message tone and intent to classify them as legitimate or suspicious. The system provides clear reasoning for its decision, flagging suspicious patterns or confirming legitimate professional language to ensure your inbox remains secure and transparent.`,
+    tags: ["React", "Tailwind","AI", "Python", "Machine Learning","grokAI-SDK","Rest API"],
     githubUrl: "https://github.com/zunair2002/phishing-ML",
     demoUrl: "/demoprojects/5",
   },
@@ -57,7 +56,7 @@ const demoprojects = [
     title: "RAG System",
     description:
       "AI Chatbot (RAG-Based) Developed an AI chatbot using Retrieval-Augmented Generation (RAG) to provide accurate and context-aware responses. Used LangChain, LangGraph, and a vector database to improve information retrieval and enhance the overall user experience.",
-    tags: ["React", "LocalStorage", "Recharts", "Tailwind"],
+    tags: ["React","TailwindCSS", "Express", "Embedding Model","VectorDB","GoogleAI-SDK"],
     githubUrl: "https://github.com/zunair2002/RAG-application",
     demoUrl: "/demoprojects/6",
   },
